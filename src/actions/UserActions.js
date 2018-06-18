0ec2933098d5c29e540c0ef8bd8391623d095bf0
Bram van Osta <bramvanosta@gmail.com>
2018-06-18 14:11:56 +0200

    Initialised login

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,6 +1,46 @@
+import moment from 'moment';
+
+import {
+  API_BASE
+} from "../config/app";
+
 export const FETCH_LOGIN_USER = '@@USER//FETCH_LOGIN';
 export const FETCH_LOGIN_USER_SUCCESS = '@@USER//FETCH_LOGIN_SUCCESS';
 export const FETCH_LOGIN_USER_ERROR = '@@USER//FETCH_LOGIN_ERROR';
 export const FETCH_LOGOUT_USER = '@@USER//FETCH_LOGOUT';
 export const FETCH_LOGOUT_USER_SUCCESS = '@@USER//FETCH_LOGOUT_SUCCESS';
 export const FETCH_LOGOUT_USER_ERROR = '@@USER//FETCH_LOGOUT_ERROR';
+
+export const loginUser = (email, password) => async (dispatch) => {
+  dispatch({
+    type: FETCH_LOGIN_USER
+  });
+
+  try {
+    const data = await fetch(`${API_BASE}/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json());
+
+    localStorage.setItem('auth', JSON.stringify({
+      token: data.token,
+      expires: moment().add(data.expires_in, 'seconds').format(),
+    }));
+
+    dispatch({
+      type: FETCH_LOGIN_USER_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_LOGIN_USER_ERROR,
+      payload: error,
+    });
+  }
+}
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,68 +1,105 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Redirect } from 'react-router-dom';
+
+import { loginUser } from '../../actions/UserActions';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       email: '',
       password: '',
     };
+
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
+  onSubmit(e) {
+    const { email, password } = this.state;
+    e.preventDefault();
 
-  handleChange(event) {
-    this.setState({
-      [event.target.id]: event.target.value,
-    });
+    this.props.loginUser(email, password);
   }
-
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  // };
 
   render() {
+    const { authenticated } = this.props;
+    const { email, password } = this.state;
+
+    if (authenticated) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div className="Login">
-      <img src="/img/logo.png" className="logo" alt="logo" />
-        {/* <form onSubmit={this.handleSubmit}> */}
-        <form>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Connexion
-          </Button>
-          <div className="forget">
-            <a href="#">J'ai oublié mon mot de passe</a>
-          </div>
-        </form>
+        <img src="/img/logo.png" className="logo" alt="Surfrider logo" />
+        <div className="Login__form">
+          <h2>Connexion</h2>
+          <form onSubmit={this.onSubmit}>
+            <div className="Login__input">
+              <label htmlFor="email">
+                <span />
+                Email
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={email}
+                onChange={event => this.setState({ email: event.target.value })}
+              />
+            </div>
+            <div className="Login__input">
+              <label htmlFor="password">
+                <span />
+                Mot de passe
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                onChange={event =>
+                  this.setState({ password: event.target.value })
+                }
+              />
+            </div>
+            <div className="Login__submit">
+              <input
+                type="submit"
+                value="Connexion"
+                disabled={email === '' || password === ''}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  loginUser: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated,
+});
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      loginUser,
+    },
+    dispatch,
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Login);
