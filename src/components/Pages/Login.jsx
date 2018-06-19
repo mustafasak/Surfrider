19a110284e9b1e3e54d94818d8df4d575fb12724
Mustafa <mustafa@loisirsencheres.com>
2018-06-19 15:37:36 +0200

    [IMPROVE] design login page

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { Redirect } from 'react-router-dom';
 
 import { loginUser } from '../../actions/UserActions';
+import '../../assets/css/Pages/Login.css';
 
 class Login extends Component {
   constructor() {
@@ -13,6 +14,8 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
+      emailFocussed: false,
+      passwordFocussed: false
     };
 
     this.onSubmit = this.onSubmit.bind(this);
@@ -27,7 +30,7 @@ class Login extends Component {
 
   render() {
     const { authenticated } = this.props;
-    const { email, password } = this.state;
+    const { email, password, emailFocussed, passwordFocussed } = this.state;
 
     if (authenticated) {
       return <Redirect to="/" />;
@@ -35,46 +38,59 @@ class Login extends Component {
 
     return (
       <div className="Login">
-        <img src="/img/logo.png" className="logo" alt="Surfrider logo" />
-        <div className="Login__form">
-          <h2>Connexion</h2>
-          <form onSubmit={this.onSubmit}>
-            <div className="Login__input">
-              <label htmlFor="email">
-                <span />
-                Email
-              </label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                value={email}
-                onChange={event => this.setState({ email: event.target.value })}
-              />
-            </div>
-            <div className="Login__input">
-              <label htmlFor="password">
-                <span />
-                Mot de passe
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={password}
-                onChange={event =>
-                  this.setState({ password: event.target.value })
-                }
-              />
-            </div>
-            <div className="Login__submit">
-              <input
-                type="submit"
-                value="Connexion"
-                disabled={email === '' || password === ''}
-              />
-            </div>
-          </form>
+        <div className="Login__container-left">
+          <figure className="Login__logo">
+            <img src="/img/Logo__white.svg" alt="Surfrider logo" />
+            <figcaption>Dashboard Surfrider</figcaption>
+          </figure>
+          <div className="Global__Filter" />
+        </div>
+        <div className="Login__container-right">
+          <div className="Login__form">
+            <h2>Connexion</h2>
+            <form onSubmit={this.onSubmit}>
+              <div className="Login__input">
+                <label htmlFor="email" className={emailFocussed || email !== '' ? 'focussed': ''}>
+                  <span>Adresse e-mail</span>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onFocus={() => this.setState({ emailFocussed: true })}
+                    onBlur={() => this.setState({ emailFocussed: false })}
+                    onChange={event => this.setState({ email: event.target.value })}
+                  />
+                </label>
+              </div>
+              <div className="Login__input">
+                <label htmlFor="password" className={passwordFocussed || password !== '' ? 'focussed': ''}>
+                  <span>Saisissez le mot de passe</span>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onFocus={() => this.setState({ passwordFocussed: true })}
+                    onBlur={() => this.setState({ passwordFocussed: false })}
+                    onChange={event =>
+                        this.setState({ password: event.target.value })
+                    }
+                  />
+                </label>
+              </div>
+              <div className="Login__submit">
+                <input
+                  type="submit"
+                  value="Connexion"
+                  /* disabled={email === '' || password === ''} */
+                />
+                <a href="#" title="#">Mot de passe oublié ?</a>
+              </div>
+            </form>
+
+            <div className="Login__lostpassword" />
+          </div>
         </div>
       </div>
     );
@@ -90,14 +106,12 @@ const mapStateToProps = state => ({
   authenticated: state.user.authenticated,
 });
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators(
+const mapDispatchToProps = dispatch => bindActionCreators(
     {
       loginUser,
     },
     dispatch,
   );
-};
 
 export default connect(
   mapStateToProps,
