69244fb89f5f59bd10bdae0d072d76b39960a9f8
Bram van Osta <bramvanosta@gmail.com>
2018-06-22 11:46:41 +0200

    Use staging API

--- src/config/app.js
+++ src/config/app.js
@@ -1,2 +1,2 @@
-// export const API_BASE = 'http://api.surfrider.bramvanosta.com';
-export const API_BASE = 'http://localhost:3000';
+export const API_BASE = 'http://api.surfrider.bramvanosta.com';
+// export const API_BASE = 'http://localhost:3000';
